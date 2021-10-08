9a98b22617310aedb8f7a8978b248af8d68fb87d
Stefanie Satna <heystef@fb.com>
2021-10-08 11:57:10 +0200

    Add getToken to retrieve Spotify Web API authentication token

--- api/app.js
+++ api/app.js
@@ -9,7 +9,7 @@ var indexRouter = require("./routes/index");
 var usersRouter = require("./routes/users");
 var testAPIRouter = require("./routes/testAPI");
 var app = express();
-var btoa = require("btoa");
+// var btoa = require("btoa");
 
 // view engine setup
 app.set("views", path.join(__dirname, "views"));
@@ -42,45 +42,45 @@ app.use(function (err, req, res, next) {
   res.render("error");
 });
 
-const APIController = (function () {
-  const clientId = "";
-  const clientSecret = "";
-  const str = clientId + ":" + clientSecret;
+// const APIController = (function () {
+//   const clientId = "";
+//   const clientSecret = "";
+//   const str = clientId + ":" + clientSecret;
 
-  const _getToken = async () => {
-    const result = await fetch("https://accounts.spotify.com/api/token", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/x-www-form-urlencoded",
-        Authorization: "Basic " + Buffer.from(str, "binary").toString("base64"),
-      },
-      body: "grant_type=client_credentials",
-    });
+//   const _getToken = async () => {
+//     const result = await fetch("https://accounts.spotify.com/api/token", {
+//       method: "POST",
+//       headers: {
+//         "Content-Type": "application/x-www-form-urlencoded",
+//         Authorization: "Basic " + Buffer.from(str, "binary").toString("base64"),
+//       },
+//       body: "grant_type=client_credentials",
+//     });
 
-    const data = await result.json();
-    return data.access_token;
-  };
+//     const data = await result.json();
+//     return data.access_token;
+//   };
 
-  const _getTrack = async (token, trackEndPoint) => {
+//   const _getTrack = async (token, trackEndPoint) => {
 
-    const result = await fetch(`${trackEndPoint}`, {
-      method: "GET",
-      headers: { "Authorization": "Bearer" + token}
-    })
+//     const result = await fetch(`${trackEndPoint}`, {
+//       method: "GET",
+//       headers: { "Authorization": "Bearer" + token}
+//     })
 
-    const data = await result.json()
-    console.log(data)
-    return data
-  }
+//     const data = await result.json()
+//     console.log(data)
+//     return data
+//   }
 
-  return {
-    getToken() {
-      return _getToken()
-    },
-    getTrack(token, trackEndPoint){
-      return _getTrack(token, trackEndPoint)
-    }
-  }
-})();
+//   return {
+//     getToken() {
+//       return _getToken()
+//     },
+//     getTrack(token, trackEndPoint){
+//       return _getTrack(token, trackEndPoint)
+//     }
+//   }
+// })();
 
 module.exports = app;
--- client/src/App.js
+++ client/src/App.js
@@ -2,27 +2,50 @@ import React, { useState, useEffect } from "react";
 import { Form } from "./Form";
 
 function App() {
-  const [apiResponse, setApiResponse] = useState("");
+  // const [apiResponse, setApiResponse] = useState("");
 
-  const callApi = async () => {
-    const res = await fetch("http://localhost:9000/testAPI");
-    const response = await res.text()
-    setApiResponse(response)
-  };
+  // const callApi = async () => {
+  //   const res = await fetch("http://localhost:9000/testAPI");
+  //   const response = await res.text()
+  //   setApiResponse(response)
+  // };
+
+  // useEffect(() => {
+  //   callApi();
+  // }, []);
+
+  const [token, setToken] = useState("");
 
   useEffect(() => {
-    callApi();
+    setToken(getToken("53bd15ac97bb4754b5722d99ef5f6b56","2989ba2bc3154aa9b117579552fa02ab"))
   }, []);
-
 
   return (
     <div>
       <header>
         <Form />
-        <p>{apiResponse}</p>
       </header>
     </div>
   );
 }
 
 export default App;
+
+async function getToken(clientId, clientSecret) {
+
+  const str = clientId + ":" + clientSecret;
+
+  const result = await fetch("https://accounts.spotify.com/api/token", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/x-www-form-urlencoded",
+      Authorization: "Basic " + Buffer.from(str, "binary").toString("base64"),
+    },
+    body: "grant_type=client_credentials",
+  });
+
+  const data = await result.json();
+  console.log(data.access_token)
+  return data.access_token;
+
+}
